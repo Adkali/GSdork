42d672f15bbd2b64ab7561bd26f7de0f2171fb6b
Adkali <admdefxs@gmail.com>
2023-01-19 23:24:11 +0200

    Fix & Improve Code.

--- GSdork.py
+++ GSdork.py
@@ -1,18 +1,23 @@
-import datetime
 import time
 import requests
 from bs4 import BeautifulSoup
-import sys
+import threading
 
-# Colors
+
+# ---------------- Esc/Results=10 ----------------
+
+EXIT_COMMAND = "esc"
+DEFAULT_RESULTS = 10
+
+# ---------------- Colors To Be Defined ----------------
+
 Yellow = "\033[1;33;40m"
 Red = "\033[1;31;40m"
 Normal = "\033[0;0m"
 BM = "\033[1;35;40m"
 
-
-# Banner Call
-def BannerShow():
+# ---------------- Banner Show ----------------
+def Banner_Show():
     print(f'''
 |  _____  |{Yellow}A{Normal}
 | |\ ___| | {Red}D{Normal}
@@ -20,14 +25,13 @@ def BannerShow():
 | | |___| | {BM}A{Normal}
 \ | |____\| L
  \|_________I 
-
+GoogleDorkQuery
     ''')
 
-
-BannerShow()
-
-
-def G_Example():
+Banner_Show()
+time.sleep(1.5)
+# ---------------- Examples Of Operators ----------------
+def G_Examples():
     print(f'''
 {Red}Operators Examples{Normal}
 >> site: [exmaple.com] filetype:[pdf]
@@ -39,55 +43,46 @@ def G_Example():
     ''')
 
 
-G_Example()
+G_Examples()
 
-while True:
+# ---------------- Function taking search query, numbers of results ----------------
+def GetSearchResult(search_query, num_results=DEFAULT_RESULTS):
     total = 0
-    Payload = input(f"Search Query ('{Yellow}esc{Normal}' for exit): ")  # Search
-    if Payload == "esc":
-        break
-    R_Number = input("Results As Possible (Default is 10): ")  # Results as number
-    Default = "10"
-    Payloads = f"q={Payload}"
-
-    Headers = {
-        "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3538.102 Safari/537.36 Edge/18.19582",
+    payload = f"q={search_query}"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3538.102 Safari/537.36 Edge/18.19582",
         "Method": "GET"
     }
-    if R_Number == Default:
-        URL = f"https://www.google.co.il/search?{Payloads}&num=10"
-    else:
-        URL = f"https://www.google.co.il/search?{Payloads}&num={R_Number}"
-    r = requests.get(URL, params=Headers)
-    H = r.headers
-    for keys, values in r.headers.items():
-        print(f"{keys},{values}")
-    if r.status_code == 200:
-        print(f"Status code:{r.status_code} - Success!\n")
-        time.sleep(2)
-    else:
+    url = f"https://www.google.co.il/search?{payload}&num={num_results}"
+    r = requests.get(url, params=headers)
+    if r.status_code != 200:
         print("Something went wrong, please try again...")
-        time.sleep(2)
-        break
-    # Using BeautifulSoup for parsing tags inside source
+        return
+
+    # ---------------- Start The Magic ----------------
     soup = BeautifulSoup(r.text, 'html.parser')
     links = soup.findAll("a", href=True)
     for link in links:
-        try:  # Get links, split them and use indexed
-            LinkHref = link.get("href")  # Href tags
-            if "url?q=" in LinkHref and not "webcache" in LinkHref:  # using if statment on 'url?q='
-                SplitLinks = LinkHref.split("/url?q=") # Split if does
+        try:
+            link_href = link.get("href")
+            if "url?q=" in link_href and not "webcache" in link_href:
+                split_links = link_href.split("/url?q=")
                 total += 1
-                print(f"{total}>> {SplitLinks[1].split('&sa=U')[0]}")  # Split again, show only links [ index 0 ]
-                time.sleep(0.5)
-                print(f"{Yellow}Results: Total of {total} URLS were found.{Normal}")
+                print(f"{total}>> {split_links[1].split('&sa=U')[0]}")
+                #time.sleep(0.5)
+        except Exception:
+            print("An error occurred, please try again...")
+    print(f"{Yellow}Results: Total of {total} URLS were found.{Normal}")
 
-        except KeyboardInterrupt:
-            print("User stopped process, exit...")
-            exit()
-
-        except IndexError:
-            if IndexError:
-                break
-            print("Out of range, try again please...")
+while True:
+    search_query = input(f"Search Query ('{Yellow}{EXIT_COMMAND}{Normal}' for exit): ")
+    if search_query == EXIT_COMMAND:
+        break
+    try:
+        Num_Results = int(input("Results As Possible (Default is 10): "))
+    except ValueError:
+        Num_Results = DEFAULT_RESULTS
+    s_thread = threading.Thread(target=GetSearchResult, args=(search_query, Num_Results))
+    #START/JOIN
+    s_thread.start()
+    s_thread.join()
